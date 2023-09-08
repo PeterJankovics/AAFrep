de836aad7925ec2bc7e29bc5600199b904cb724d
Jankovics Péter <jankovicsp21@ADY-NAGYATAD.SULINET.HU>
2023-09-08 10:02:19 +0200

    elso1

--- elso/elso/Program.cs
+++ elso/elso/Program.cs
@@ -10,6 +10,19 @@ namespace elso
     {
         static void Main(string[] args)
         {
+            Console.WriteLine("Hello world!");
+            int szam = 6; Console.WriteLine(szam);
+            int mszam = -6; Console.WriteLine(mszam);
+            double tszam = 12.5; Console.WriteLine(tszam);
+            char betu = 'v'; Console.WriteLine(betu);
+            Console.WriteLine((char)(betu + 2));
+            string szo = "asd"; Console.WriteLine(szo);
+            Console.WriteLine(3 / 1.12);
+            int i = 0;
+            Console.WriteLine(++i);
+
+            
+
         }
     }
 }

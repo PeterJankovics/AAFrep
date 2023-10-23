08f5cdae56c09c34a638d6b67bb1bc7f34cd34e9
PeterJankovics <jankovics.peter.21@ady-nagyatad.hugit config --global user.email jankovics.peter.21@ady-nagyatad.hugit config --global user.email jankovics.peter.21@ady-nagyatad.hu>
2023-10-23 20:13:09 +0200

    hf

---
+++ gyakorlas/hf/hf/Form1.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace hf
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            Console.WriteLine("kerek egy szoveget");
+            string szoveg = Console.ReadLine();
+            string[] szavak = szoveg.Split(' ');
+            for (int i = 0; i < szavak.Length; i++)
+            {
+                if (szavak[i] != "")
+                {
+                    szavak[i] = char.ToUpper(szavak[i][0]) + szavak[i].Substring(1);
+                }
+            }
+
+        }
+    }
+}

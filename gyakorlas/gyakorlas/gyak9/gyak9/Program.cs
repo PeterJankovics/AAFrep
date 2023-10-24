d6e8dddfeb83e8c96832e1054915a19d78db043a
Jankovics Péter <jankovicsp21@ADY-NAGYATAD.SULINET.HU>
2023-10-24 12:35:15 +0200

    ada

---
+++ gyakorlas/gyakorlas/gyak9/gyak9/Program.cs
@@ -0,0 +1,17 @@
+﻿// bekeni egy szot vegignezni betunkent minden betut ellenorzunk hogy betu e
+// ha igen akkor folytatjuk a kovetkezovel ha nincs bennen akkor kiirjuk hogy nem szo.
+string szavak = "";
+Console.WriteLine("kerek egy szot!! ");
+string betuk = "qwertzuiopőúöüóasdfghjkléáűíyxcvbnm";
+
+while (szavak == "")
+{
+    szavak = Console.ReadLine();
+    for (int i = 0; i < szavak.Length; i++)
+    {
+        for (int j = 0; i <= betuk.Length; j++) 
+        {
+
+        }
+    }
+}

48386618fe9631ebd352c4cffb83bf2b61dbeb26
PeterJankovics <jankovics.peter.21@ady-nagyatad.hugit config --global user.email jankovics.peter.21@ady-nagyatad.hugit config --global user.email jankovics.peter.21@ady-nagyatad.hu>
2023-11-05 18:11:10 +0100

    hazi

---
+++ hf/hazi/hazi/Program.cs
@@ -0,0 +1,34 @@
+﻿using System.Runtime.InteropServices;
+
+var random = new Random();
+string betuk = "qwertzuioöüópőúasdfghjkléáűíyxcvbnm";
+for (int i = 0; i < 5; i++)
+{
+    string szo = "";
+    for (int e = 0; e < 5; e++)
+    {
+        szo += betuk[random.Next(betuk.Length)];
+    }
+    Console.WriteLine(szo);
+}
+
+int darab = 10000;
+int hossz = random.Next(4, 7);
+string[] szavak = new string[darab];
+for (int i = 0; i < darab; i++)
+{
+    string szo2 = "";
+    for (int e = 0; e < 5; e++)
+    {
+        szavak[e] += betuk[random.Next(betuk.Length)];
+    }
+}
+File.WriteAllLines("szavak.txt", szavak);
+
+string[] words = File.ReadAllLines("szavak.txt");
+
+string azonos = "";
+for (int i = 0; i < szavak.Length; i++)
+{
+    
+}

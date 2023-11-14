90a5b6f25609c446b4b94b4f8696c2093174d193
Jankovics Péter <jankovicsp21@ADY-NAGYATAD.SULINET.HU>
2023-11-14 14:03:59 +0100

    lista

---
+++ lista/lista1/lista1/Program.cs
@@ -0,0 +1,40 @@
+﻿List<string> szavak = new List<string>();
+szavak.Add("Kolos");
+szavak.Add("Dani");
+szavak.Add("Dominik");
+szavak.Add("Anna");
+Console.WriteLine(szavak);
+
+for (int i = 0; i < szavak.Count; i++)
+{
+    Console.WriteLine(szavak[i]);
+}
+Console.WriteLine("--------");
+foreach (string s in szavak)
+{
+    Console.WriteLine(s);
+}
+Console.WriteLine("-----");
+Console.WriteLine(szavak.IndexOf("Kolos"));
+
+Dictionary<string,int> p = new Dictionary<string,int>();
+p.Add("Laci", 3);
+p.Add("Bence", 0);
+Console.WriteLine(p.Count);
+p["Tomi"] = 4;
+Console.WriteLine(p.Count);
+
+try
+{
+    p.Add("bence", 10);
+}
+catch (Exception)
+{
+    p["Bence"]--;
+}
+p.Remove("Bence");
+//Console.WriteLine(p["Bence"]);
+foreach (KeyValuePair e in p)
+{
+    
+}

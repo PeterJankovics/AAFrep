13a2f188219f974e698adc4afb27780340fb583b
Jankovics Péter <jankovicsp21@ADY-NAGYATAD.SULINET.HU>
2023-11-21 08:05:42 +0100

    fel

---
+++ lista/lista2/lista2/Program.cs
@@ -0,0 +1,22 @@
+﻿// csinalni egy listat amiben véletlen szamokat fogunk generalni. (ket jegyu szamok)
+// megszamolni hogy az egyes szamokbol hany darab volt.
+
+List<int> szamok = new List<int>();
+ Random rand  = new Random();
+int darab = rand.Next(10000, 100000);
+for (int i = 0; i < darab; i++)
+{
+    szamok.Add(rand.Next(10, 100));
+}
+Console.WriteLine("{0} darab szám", szamok.Count());
+
+Dictionary<int, int> stat= new Dictionary<int, int>();  
+
+for (int i = 0; i < szamok.Count; i++)
+{
+    //szamok[i] sza aktualis elem.
+    if (stat.ContainsKey(szamok[i]))
+    {
+        
+    }
+}

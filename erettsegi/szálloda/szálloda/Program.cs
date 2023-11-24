8af0b4441f34ac961cd512b5d3d708d11774c126
Jankovics Péter <jankovicsp21@ADY-NAGYATAD.SULINET.HU>
2023-11-24 11:29:37 +0100

    szalloda

--- erettsegi/szálloda/szálloda/Program.cs
+++ erettsegi/szálloda/szálloda/Program.cs
@@ -16,6 +16,18 @@
     }
     return szobaar * ejdb;
 }
+static int melyikhonap(int napdb, List<int> holkezdodik)
+{
+    for (int i = 0; i< holkezdodik.Count;i++) 
+    {
+        if (napdb < holkezdodik[i])
+        {
+            return i + 1;
+        }
+
+    }
+    return 12;
+}
 
 StreamReader olvas = new StreamReader("pitypang.txt");
 olvas.ReadLine();
@@ -59,11 +71,47 @@ for (int i = 0; i < sorszamok.Count; i++)
 Console.WriteLine("2. feladat: ");
 Console.WriteLine("{0} ({1}) - {2}", vendeg[maxId], erkezesnapja[maxId], maxEj);
 
+int osszeg = 0;
 StreamWriter ir = new StreamWriter("bevetel.txt");
+
 for (int i = 0;i < sorszamok.Count;i++)
 {
-    ir.WriteLine("{0}:{1}", sorszamok[i], 
-        fizetendo(erkezesnapja[i], tavozasnapja[i], vendegszam[i], etkezes[i]));
+    int fiz = fizetendo(erkezesnapja[i], tavozasnapja[i], vendegszam[i], etkezes[i]);
+    ir.WriteLine("{0}:{1}", sorszamok[i], fiz);
+    osszeg += fiz;
 }
 
 ir.Close();
+Console.WriteLine("3. feladat: Az éves bevétel {0:n0} Ft.", osszeg);
+
+olvas = new StreamReader("honapok.txt");
+List<string> honapok = new List<string>();
+List<int> napszam = new List<int>();
+List<int> holKezdoik = new List<int>();
+int mutato = 0;
+while (olvas.EndOfStream)
+{
+    string sor = olvas.ReadLine();
+    if (mutato == 0)
+    {
+        honapok.Add(sor);
+    }
+    else if (mutato == 1)
+    {
+        napszam.Add(Convert.ToInt32(sor));
+    }
+    else
+    {
+        holKezdoik.Add(Convert.ToInt32(sor));
+        mutato = -1;
+    }
+    mutato++;
+}
+
+olvas.Close();
+Dictionary <int, int> vendegejszakak = new Dictionary<int, int>();
+for (int i = 0; i < sorszamok.Count; i++)
+{
+    Console.WriteLine(erkezesnapja[i] + " - " + tavozasnapja[i]);
+}
+Console.WriteLine(melyikhonap(1,holKezdoik));

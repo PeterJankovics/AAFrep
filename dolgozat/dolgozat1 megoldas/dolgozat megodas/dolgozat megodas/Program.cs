a5b689628a15322886905bdfeb6d30e391667ac6
Jankovics Péter <jankovicsp21@ADY-NAGYATAD.SULINET.HU>
2023-12-01 08:44:44 +0100

    dolg meg

---
+++ dolgozat/dolgozat1 megoldas/dolgozat megodas/dolgozat megodas/Program.cs
@@ -0,0 +1,60 @@
+﻿List<int> percek = new List<int>();
+List<double> ho1 = new List<double>();
+List<double> ho2 = new List<double>();
+List<double> ho3 = new List<double>();
+List<double> ho4 = new List<double>();
+List<int> darab = new List<int>();
+
+// elso megoldas
+void betolt()
+{
+    StreamReader olvas = new StreamReader("adathalmaz.csv");
+    olvas.ReadLine();
+    while (!olvas.EndOfStream)
+    {
+        string sor = olvas.ReadLine();
+        string[] vag = sor.Split(";");
+        percek.Add(int.Parse(vag[0]));
+        ho1.Add(double.Parse(vag[1]));
+        ho2.Add(double.Parse(vag[2]));
+        ho3.Add(double.Parse(vag[3]));
+        ho4.Add(double.Parse(vag[4]));
+        darab.Add(int.Parse(vag[5]));
+    }
+    Console.WriteLine(darab.Count);
+
+    olvas.Close();
+}
+// masodik megoldas
+void betolt2()
+{
+    string[] sorok = File.ReadAllLines("adathalmaz.csv");
+    for (int i = 1; i < sorok.Length; i++)
+    {
+        string[] vag = sorok[i].Split(";");
+        percek.Add(int.Parse(vag[0]));
+        ho1.Add(double.Parse(vag[1]));
+        ho2.Add(double.Parse(vag[2]));
+        ho3.Add(double.Parse(vag[3]));
+        ho4.Add(double.Parse(vag[4]));
+        darab.Add(int.Parse(vag[5]));
+    }
+}
+void betolt3()
+{
+    string[] sorok = File.ReadAllLines("adathalmaz.csv");
+    foreach (string sor in sorok)
+    {
+        string[] vag = sor.Split(";");
+        percek.Add(int.Parse(vag[0]));
+        ho1.Add(double.Parse(vag[1]));
+        ho2.Add(double.Parse(vag[2]));
+        ho3.Add(double.Parse(vag[3]));
+        ho4.Add(double.Parse(vag[4]));
+        darab.Add(int.Parse(vag[5]));
+    }
+}
+
+
+betolt2();
+betolt();

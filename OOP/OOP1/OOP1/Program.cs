e84a744a30a8862dc772888a40db691bf9f012e1
Jankovics Péter <jankovicsp21@ADY-NAGYATAD.SULINET.HU>
2023-12-12 14:05:06 +0100

    OOP1

---
+++ OOP/OOP1/OOP1/Program.cs
@@ -0,0 +1,34 @@
+﻿class Auto
+{
+    public int kerekszam = 4;
+    public void dudal()
+    {
+        Console.WriteLine("tututttttttuuuuuuu");
+    }
+    private string _tulaj;
+    public string tulajdonos
+    {
+        get
+        {
+            return _tulaj;
+        }
+        set
+        {
+            _tulaj = value;
+        }
+    }
+}
+class Program
+{
+    static void Main()
+    {
+        Auto kocsi = new Auto();
+        Console.WriteLine(kocsi.kerekszam);
+        kocsi.dudal();
+        kocsi.tulajdonos = "Gabi";
+        Console.WriteLine(kocsi.tulajdonos);
+        kocsi.tulajdonos = "Bence";
+        Console.WriteLine(kocsi.tulajdonos);
+
+    }
+}
---
+++ erettsegi/gyak1/gyak1/Program.cs
@@ -0,0 +1,38 @@
+﻿List<string> adatok1 = new List<string>();
+List<string> adatok2 = new List<string>();
+string[] olvastomb = File.ReadAllLines("kiserlet.txt");
+for (int i = 0; i < olvastomb.Length; i++)
+{
+    adatok1.Add(olvastomb[i]);
+    //Console.Write(olvastomb[i]);
+
+}
+
+StreamReader beol = new StreamReader("kiserlet.txt");
+while (!beol.EndOfStream)
+{
+    adatok2.Add(beol.ReadLine());
+
+}
+beol.Close();
+
+for (int i = 0; i < adatok1.Count; i++)
+{
+    Console.WriteLine("1.{0}, 2.{1}",adatok1[i], adatok2[i]);
+}
+
+
+Random random = new Random();
+
+string fej = "fej";
+string iras = "iras";
+
+int fejvagyiras = random.Next(1);
+if (fejvagyiras > 0)
+{
+    Console.WriteLine(fej);
+}
+else
+{
+    Console.WriteLine(iras);
+}

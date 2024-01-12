565647e15df614a9d3409d2f4e64998e9976fbf9
Jankovics Péter <jankovicsp21@ADY-NAGYATAD.SULINET.HU>
2024-01-12 08:44:41 +0100

    asd

---
+++ OOP/kopapirollo/kopapirollo/Program.cs
@@ -0,0 +1,6 @@
+﻿using kopapirollo;
+
+jatekos j1 = new jatekos();
+Console.WriteLine(j1.valasztas);
+jatekos j2 = new jatekos();
+Console.WriteLine(j2.valasztas);
---
+++ OOP/kopapirollo/kopapirollo/jatekos.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace kopapirollo
+{
+    internal class jatekos
+    {
+        string[] lehetosegek = new string[] {"ko","papir","ollo"};
+        public string valasztas;
+        public jatekos()
+        {
+            Random rand = new Random();
+            valasztas = lehetosegek[rand.Next(lehetosegek.Length)];
+        }
+        public void kinyer(jatekos masik)
+        {
+            if (valasztas == masik.valasztas)
+            {
+                Console.WriteLine("döntetlen");
+            }
+            else
+            {
+
+            }
+        }
+    }
+}

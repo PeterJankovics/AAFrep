7a565081925e5747153fdae3584cfdb927c929de
Jankovics Péter <jankovicsp21@ADY-NAGYATAD.SULINET.HU>
2024-01-19 10:17:19 +0100

    asd

--- AB/gyak4/spl.sql
+++ AB/gyak4/spl.sql
@@ -10,4 +10,20 @@ CREATE TABLE oktato2tantargy (id int (10) NOT NULL AUTO_INCREMENT PRIMARY KEY, o
 CREATE TABLE oktato2tantargy (id int (10) NOT NULL AUTO_INCREMENT PRIMARY KEY, evfolyam INT, betujel VARCHAR(10));
 CREATE TABLE oktato2tantargy (id int (10) NOT NULL AUTO_INCREMENT PRIMARY KEY, datum DATE, ora int, osztalyid int, oktato2tantargyid int);
 
+CREATE TABLE eredeti(
+	id int AUTO_INCREMENT primary KEY,
+	ora int(10),
+	osztaly VARCHAR (50),
+	agazat VARCHAR(50),
+	agazta VARCHAR(50),
+	tantargy VARCHAR(50),
+	oktato VARCHAR(50),
+	ofo VARCHAR(50),
+	datum DATE
+);
+INSERT INTO eredeti (ora, osztaly, agazat, tantargy, oktato, ofo, datum)
+VALUES(
+
+);
+
 

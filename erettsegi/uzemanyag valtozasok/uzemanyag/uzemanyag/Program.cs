a453f32cf915067defa6542281a025abfef05e0b
Jankovics Péter <jankovicsp21@ADY-NAGYATAD.SULINET.HU>
2024-01-23 14:04:50 +0100

    adas

---
+++ erettsegi/uzemanyag valtozasok/uzemanyag/uzemanyag/Program.cs
@@ -0,0 +1,69 @@
+﻿using Microsoft.Win32.SafeHandles;
+using uzemanyag;
+
+List<adatok> valtozasok = new List<adatok>();
+
+StreamReader olvas = new StreamReader("uzemanyag.txt");
+
+while (!olvas.EndOfStream)
+{
+    valtozasok.Add(new adatok(olvas.ReadLine()));
+}
+
+olvas.Close();
+
+Console.WriteLine("3. Feladat: Valtozasok szama: {0}", valtozasok.Count());
+
+Console.WriteLine("4. Feladat:");
+adatok minimum = valtozasok[0]; 
+int mindarab = 0;
+
+
+for (int i = 0; i < valtozasok.Count; i++)
+{
+    if (valtozasok[i].kulonbseg() < minimum.kulonbseg())
+    {
+        minimum = valtozasok[i];
+    }
+}
+
+for (int i = 0; i < valtozasok.Count; i++)
+{
+    if (valtozasok[i].kulonbseg() == minimum.kulonbseg())
+    {
+        mindarab++;
+    }
+}
+
+Console.WriteLine("A legkisebb kulonbseg: {0}", minimum.kulonbseg());
+
+Console.WriteLine("5. Feladat: a legkisebb kulonbseg eloforsulasa: {0}", mindarab);
+
+bool volt = false;
+
+for (int i = 0; i < valtozasok.Count; i++)
+{
+    if (DateTime.IsLeapYear(valtozasok[i].datum.Year))
+    {
+        if (valtozasok[i].datum.Month == 2 && valtozasok[i].datum.Day == 24)
+        {
+            volt = true;
+            break;
+        }
+    }
+}
+
+if (volt)
+{
+    Console.WriteLine("6. Feladat: volt valtozas szokonapon.");
+}
+else
+{
+    Console.WriteLine("6. Feladat: nem volt valtozas szokonapon.");
+}
+
+
+for (int i = 0; i < valtozasok.Count; i++)
+{
+    Console.WriteLine(valtozasok[i].fileba());
+}
---
+++ erettsegi/uzemanyag valtozasok/uzemanyag/uzemanyag/adatok.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace uzemanyag
+{
+    internal class adatok
+    {
+        public DateTime datum;
+        public int benzin;
+        public int gazolaj;
+        double euro = 307.7;
+
+        public adatok(string sor)
+        {
+            string[] vag = sor.Split(";");
+            datum = Convert.ToDateTime(vag[0]);
+            benzin = Convert.ToInt32(vag[1]);
+            gazolaj = Convert.ToInt32(vag[2]);
+
+        }
+        public int kulonbseg()
+        {
+            return Math.Abs(benzin - gazolaj);
+        }
+        public double benzineuro()
+        {
+            return benzin / euro;
+        }
+        public double gazolajeuro()
+        {
+            return gazolaj / euro;
+        }
+        public string fileba()
+        {
+            return datum.ToString() + ";" +Math.Round( benzineuro(),2) + ";" + Math.Round( gazolajeuro(),2);
+        }
+    }
+}

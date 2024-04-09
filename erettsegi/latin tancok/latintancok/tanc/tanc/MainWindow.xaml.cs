473c792295523f1c02d3ed297b709affaa624ad2
Jankovics Péter <jankovicsp21@ady-nagyatad.sulinet.hu>
2024-04-09 09:37:55 +0200

    sdsd

---
+++ erettsegi/latin tancok/latintancok/tanc/tanc/MainWindow.xaml.cs
@@ -0,0 +1,82 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace tanc
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        List<Par> tancok = new List<Par>();
+
+        private void Window_Loaded(object sender, RoutedEventArgs e)
+        {
+            StreamReader olvas = new StreamReader("tancrend.txt");
+            while (!olvas.EndOfStream)
+            {
+                string[] sorok = new string[3];
+                for (int i = 0; i < sorok.Length; i++)
+                {
+                    sorok[i] = olvas.ReadLine();
+                }
+                tancok.Add(new Par(sorok[0], sorok[1], sorok[2]));
+            }
+
+
+       
+
+            olvas.Close();
+
+            List<string> tancNevek = new List<string>();
+            for (int i = 0; i < tancok.Count; i++)
+            {
+                if (tancNevek.Contains(tancok[i].tanc))
+                {
+                    tancNevek.Add(tancok[i]);
+                }
+            }
+            listBox
+        }
+
+        private void button_Click(object sender, RoutedEventArgs e)
+        {
+            textBlock.Text = "Első tánc: " + tancok[0].tanc + ", utolsó tánc: " + tancok.Last().tanc + ".";
+        }
+
+        private void button1_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+        private int tancszamol(string nev)
+        {
+            int db = 0;
+            for (int i = 0; i < tancok.Count; i++)
+            {
+                if (tancok[i].tanc == nev)
+                    db++;
+            }
+            return db;
+        }
+
+
+    }
+}
---
+++ erettsegi/latin tancok/latintancok/tanc/tanc/Par.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace tanciskolawpf
+{
+    internal class Par
+    {
+        public string tanc;
+        public string lany;
+        public string fiu;
+        public Par(string tanc, string lany, string fiu)
+        {
+            this.tanc = tanc;
+            this.lany = lany;
+            this.fiu = fiu;
+        }
+    }
+}
